cd65dca4bb70d1bc6ef6fe7a15252af3c730d860
smokwid20 <taikhoangchung9196@gmail.com>
2018-03-16 19:58:14 +0700

    adding audio player

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function togglePause() {
+
+    var playButton = document.getElementsByClassName("player-control-play")
+    playButton.inner = '<i class="far fa-pause-circle"></i>';
+}
+console.log("test");
