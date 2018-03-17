7b8f498fa89d187928165a7d2a291b60c7643561
smokwid20 <taikhoangchung9196@gmail.com>
2018-03-17 20:04:49 +0700

    add test file,playButton is working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
-function togglePause() {
 
-    var playButton = document.getElementsByClassName("player-control-play")
-    playButton.inner = '<i class="far fa-pause-circle"></i>';
+
+function togglePlayPause(){   
+    var myAudio = document.getElementById("player");
+    var playButton = document.getElementById("play-button");
+
+    if(myAudio.paused){
+        playButton.classList.remove("far","fa-play-circle");
+        playButton.classList.add("far","fa-pause-circle")
+        myAudio.play();
+    }else{
+        myAudio.pause();
+        playButton.classList.remove("far","fa-pause-circle");
+        playButton.classList.add("far","fa-play-circle")
+    }
 }
+
 console.log("test");
---
+++ test.js
@@ -0,0 +1,9 @@
+var vid = document.getElementById("myaudio");
+
+function playVid() {
+    vid.play();
+}
+
+function pauseVid() {
+    vid.pause();
+}
