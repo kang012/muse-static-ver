0c13aab3818944fb7296caf2523158afd11f72f3
smokwid20 <taikhoangchung9196@gmail.com>
2018-03-18 14:55:30 +0700

    fixed playbutton

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-
-
 function togglePlayPause(){   
     var myAudio = document.getElementById("player");
     var playButton = document.getElementById("play-button");
@@ -8,11 +6,10 @@ function togglePlayPause(){
         playButton.classList.remove("far","fa-play-circle");
         playButton.classList.add("far","fa-pause-circle")
         myAudio.play();
-    }else{
+    }else if (myAudio.play || myAudio.ended){
         myAudio.pause();
         playButton.classList.remove("far","fa-pause-circle");
-        playButton.classList.add("far","fa-play-circle")
+        playButton.classList.add("far","fa-play-circle");
     }
 }
 
-console.log("test");
