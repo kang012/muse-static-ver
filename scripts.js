7fa86526b8438efdf366ba3269f8302f8c362d30
mihawk <mihawk@mihawks-iMac.local>
2018-03-18 22:00:34 +0700

    progressbar is not working

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
-function togglePlayPause(){   
-    var myAudio = document.getElementById("player");
-    var playButton = document.getElementById("play-button");
+function togglePlayPause() {
+  var myAudio = document.getElementById("player");
+  var playButton = document.getElementById("play-button");
 
-    if(myAudio.paused){
-        playButton.classList.remove("far","fa-play-circle");
-        playButton.classList.add("far","fa-pause-circle")
-        myAudio.play();
-    }else if (myAudio.play || myAudio.ended){
-        myAudio.pause();
-        playButton.classList.remove("far","fa-pause-circle");
-        playButton.classList.add("far","fa-play-circle");
+  if (myAudio.paused) {
+    if (myAudio.ended) {
+      playButton.classList.remove("far", "fa-pause-circle");
+      playButton.classList.add("far", "fa-play-circle");
+      console.log(myAudio.ended);
+    } else {
+      playButton.classList.remove("far", "fa-play-circle");
+      playButton.classList.add("far", "fa-pause-circle");
+      myAudio.play();
     }
+  } else if (myAudio.play) {
+    myAudio.pause();
+    playButton.classList.remove("far", "fa-pause-circle");
+    playButton.classList.add("far", "fa-play-circle");
+  }
 }
-
--- test.js
+++ test.js
@@ -1,9 +1,38 @@
-var vid = document.getElementById("myaudio");
+function togglePlayPause() {
+  var myAudio = document.getElementById("player");
+  var playButton = document.getElementById("play-button");
 
-function playVid() {
-    vid.play();
+  if (myAudio.paused) {
+    if (myAudio.ended) {
+      playButton.classList.remove("far", "fa-pause-circle");
+      playButton.classList.add("far", "fa-play-circle");
+      console.log(myAudio.ended);
+    } else {
+      playButton.classList.remove("far", "fa-play-circle");
+      playButton.classList.add("far", "fa-pause-circle");
+      myAudio.play();
+    }
+  } else if (myAudio.play) {
+    myAudio.pause();
+    playButton.classList.remove("far", "fa-pause-circle");
+    playButton.classList.add("far", "fa-play-circle");
+  }
 }
+function trackingPlayer() {
+  console.log("Audio is playing");
+  var myAudio = document.getElementById("player");
+  var progressBar;
+  var pin;
+  var currentTime = myAudio.currentTime;
+  var songDuration = myAudio.duration;
+  var temp;
 
-function pauseVid() {
-    vid.pause();
+  while (!myAudio.paused) {
+    // progressBar = document.getElementById("audio-progress");
+    // pin = document.getElementById("audio-slider");
+    currentTime = myAudio.currentTime;
+    songDuration = myAudio.duration;
+    temp = (currentTime * 100 / songDuration) + "%";
+    console.log(temp);
+  }
 }
